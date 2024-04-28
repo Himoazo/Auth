3539771a0abdfb24ca093ca4b1cbabdc0aa7ff39
Hoomam <hoaz1600@student.miun.se>
2024-04-28 21:35:47 +0200

    first commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,46 @@
+/**
+ * Authenticating routing
+ */
+const express = require("express");
+const router = express.Router();
+
+//Registrera användare
+router.post("/register", async (req, res)=>{
+    try{
+        const {username, password, email} = req.body;
+
+        //Validation
+        if(!username || !password || !email){
+            return res.status(400).json({error: "Invalid input"});
+        }
+
+        //Successful user registration
+        res.status(201).json({message: "User is created"});
+    }catch(error){
+        res.status(500).json({message: "Internal server error"});
+    }
+});
+
+//logga in användare
+router.post("/login", async (req, res)=>{
+    try{
+        const {username, password} = req.body;
+
+        //Validation
+        if(!username || !password){
+            return res.status(400).json({error: "Invalid username/password"});
+        }
+
+        //Check credentials
+        if(username === "Himo" && password === "Azo"){
+            res.status(200).json({message: "logged in successfully"});
+        }else{
+            res.status(401).json({error: "Invalid username/password"});
+        }
+        
+    }catch(error){
+        res.status(500).json({message: "Internal server error"});
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT;
+
+//Routing
+app.use("/api", authRoutes);
+
+//Starting app
+app.listen(port, ()=>{
+    console.log(`Serve is running on port : ${port}`);
+});
